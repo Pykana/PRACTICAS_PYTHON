af4d1a949bb11f976d72525bcc949e664d6c66fc
Eduardo Elias <Eduardo.Elias.1998@protonmail.com>
2024-09-02 11:40:29 -0500

    avance

---
+++ Prueba_02/main.py
@@ -0,0 +1,19 @@
+# USO DEL IF - DESCUENTO 
+
+# Ejemplo 1 
+
+nombre=input("Escriba su nombre : ")
+TotalCompras= float(input("Escriba el total de comrpas realizadas : "))
+if(TotalCompras >500):
+    descuento=TotalCompras*.10  #10%
+    print("Felicidades ",nombre," tiene un total de ",descuento," por compras mayores a 500")
+else:
+    print("No cuenta con el monto requerido (500) para obtener el descuento")
+
+# Ejemplo 2
+
+nombre=input("Escriba su nombre : ")
+edad=int(input("Escriba su edad : "))
+
+
+
