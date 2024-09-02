3853a9fd52177dbd0c4191b2e1c3c0be5b370821
Eduardo Elias <Eduardo.Elias.1998@protonmail.com>
2024-09-02 09:52:50 -0500

    pruebas 1

---
+++ Prueba_01/main.py
@@ -0,0 +1,24 @@
+# IMPRIMIR DATA
+# print("hola gente")
+
+# SETEAR VARIABLES
+# var1="hola"
+# var2=" gente"
+# var3=var1 + var2
+# var4="Otra"
+# var4+=" Forma"
+
+# print(var1+var2)
+# print(var3)
+# print(var4)
+
+# BUSCAR SUB CADENA DENTRO DE TEXTO
+
+# mensaje="Cadena de mensaje en Python ,hola mundo"
+# subCadena=mensaje.find("Python")
+
+# print("Mensaje :\n" ,mensaje ,"\ncon subcadena: (posicion) " ,subCadena)
+
+
+# EXTRAER MENSAJE DE LA POSICION DE LA SUBCADENA
+
