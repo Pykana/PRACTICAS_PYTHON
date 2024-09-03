3cefc0afb88557f51f29913894fa3804ece70924
Eduardo Elias <Eduardo.Elias.1998@protonmail.com>
2024-09-03 10:43:03 -0500

    avance

---
+++ Prueba_03/main.py
@@ -0,0 +1,11 @@
+#Ejemplo 1
+
+#convertir a mayusculas y minisculas
+
+texto=input("Ingrese texto a convertir: ")
+print(f'Texto en Mayusculas {texto.upper()}')
+print(f'Texto en Minusculas {texto.lower()}')
+
+
+#Ejemplo 2
+
